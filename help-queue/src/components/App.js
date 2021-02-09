3df0293585154da82a7c1f286103f13b0de4973b
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-08 20:46:51 -0800

    update project

---
+++ help-queue/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TicketList from "./TicketList";
+import Header from "./Header";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <TicketList />
+    </React.Fragment>
+  );
+}
+
+
+export default App;
