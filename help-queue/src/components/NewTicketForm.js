f56259edb4021a76804640bc94126baf21c95ea2
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-09 21:17:01 -0800

    successfully implement form and masterlist

--- help-queue/src/components/NewTicketForm.js
+++ help-queue/src/components/NewTicketForm.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
-function NewTicketForm(){
+function NewTicketForm(props){
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
   }
   return (
     <React.Fragment>
@@ -18,7 +17,7 @@ function NewTicketForm(){
           placeholder='Pair Names' />
         <input
           type='text'
-          name='names'
+          name='location'
           placeholder='Location' />
         <textarea 
           name='issue'
@@ -28,5 +27,9 @@ function NewTicketForm(){
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- help-queue/src/components/TicketControl.js
+++ help-queue/src/components/TicketControl.js
@@ -23,11 +23,18 @@ class TicketControl extends React.Component {
     this.setState(prevState => ({formVisibleOnPage: !prevState.formVisibleOnPage}));
   }
 
+  handleAddingNewTicketToList = (newTicket) => { const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+  this.setState({
+    masterTicketList: newMasterTicketList,
+    formVisibleOnPage: false
+  });
+  }
+
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       buttonText = "Return to Ticket List";
     } else {
       currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} />;
